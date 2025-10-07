827b644e4ff6884609c683a6daaac47c4ac45b25
dachacond <102201222+dachacond@users.noreply.github.com>
2025-10-07 18:53:45 -0500

    chore: remove terraform artifacts; add .gitignore; update CI to use GitHub OIDC role

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,8 +9,8 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
       source  = "hashicorp/aws"
+      version = ">= 4.0.0, < 6.0.0"
     }
   }
 }
