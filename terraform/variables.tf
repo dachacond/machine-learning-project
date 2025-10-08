f09020792128cac1e71489d4a93e8c44bf785557
dachacond <102201222+dachacond@users.noreply.github.com>
2025-10-07 19:20:19 -0500

    chore: remove terraform artifacts; add .gitignore; update CI to use GitHub OIDC role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,9 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "kms_key_arn" {
+  description = "ARN of pre-created KMS key to use for resources (set to empty to allow module to create keys)"
+  type        = string
+  default     = ""
+}
